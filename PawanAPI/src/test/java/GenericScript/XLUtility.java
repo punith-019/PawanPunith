993035e06630f1c543bc3c9e4bdae5c0b97d67bd
DELL <DELL@DESKTOP-4VO602L>
2023-02-09 11:32:55 +0530

    push

---
+++ PawanAPI/src/test/java/GenericScript/XLUtility.java
@@ -0,0 +1,12 @@
+package GenericScript;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class XLUtility
+{
+	public static FileInputStream fi;
+	public static FileOutputStream fo;
+	
+
+}
